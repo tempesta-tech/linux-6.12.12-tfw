3d3d07bfdfa874afb4e4bf7658f112295a08aed1
Constantine <kt@tempesta-tech.com>
2025-05-12 12:02:28 +0300

    fix: remove unnecessary condition
    `rcu_dereference()` is tolerant to null arguments, we
    don't have to protect it

--- net/ipv6/tcp_ipv6.c
+++ net/ipv6/tcp_ipv6.c
@@ -561,11 +561,7 @@ static int tcp_v6_send_synack(const struct sock *sk, struct dst_entry *dst,
 
 		rcu_read_lock();
 		opt = ireq->ipv6_opt;
-#ifdef CONFIG_SECURITY_TEMPESTA
-		if (!sock_flag(sk, SOCK_TEMPESTA) && !opt)
-#else
 		if (!opt)
-#endif
 			opt = rcu_dereference(np->opt);
 		err = ip6_xmit(sk, skb, fl6, skb->mark ? : READ_ONCE(sk->sk_mark),
 			       opt, tclass, READ_ONCE(sk->sk_priority));
@@ -1491,11 +1487,7 @@ static struct sock *tcp_v6_syn_recv_sock(const struct sock *sk, struct sk_buff *
 	   to newsk.
 	 */
 	opt = ireq->ipv6_opt;
-#ifdef CONFIG_SECURITY_TEMPESTA
-	if (!sock_flag(sk, SOCK_TEMPESTA) && !opt)
-#else
 	if (!opt)
-#endif
 		opt = rcu_dereference(np->opt);
 	if (opt) {
 		opt = ipv6_dup_options(newsk, opt);
